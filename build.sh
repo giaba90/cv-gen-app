52884943396caecdd6e5adefc4443740206dadfd
Gianluca Barranca <info@gianlucabarranca.it>
2024-08-15 13:15:40 +0200

    update node

--- build.sh
+++ build.sh
@@ -9,18 +9,31 @@ if ! [ -x "$(command -v npm)" ]; then
   exit 1
 fi
 
-# 2. Esegui il build della webapp con Vite
-echo "Mostra la versione di node"
+# 2. Aggiorna node
+# Rimuovi la vecchia versione di Node.js (opzionale)
+apt remove nodejs
+
+# Aggiungi il repository di Node.js per la versione 18.x
+curl -fsSL https://deb.nodesource.com/setup_18.x | -E bash -
+
+# Installa Node.js
+apt install -y nodejs
+
+# Verifica la versione di Node.js e npm
 node -v
+npm -v
+
+
+# 3. Esegui il build della webapp con Vite
 echo "Building the webapp using Vite..."
 npm install
 npm run build
 
 
-# 5. Sposta il contenuto delle cartelle indietro di una directory
+# 4. Sposta il contenuto delle cartelle indietro di una directory
 echo "Sposta il contenuto della cartella dist/ nella root del sito"
 mv dist/* ../
 
-# 6. Conferma lo spostamento riuscito
+# 5. Conferma lo spostamento riuscito
 echo "Contenuto spostato con successo."
 
