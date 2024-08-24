e9c83a56c80202e4474c53acbb64fe8254cfc7f9
Gianluca Barranca <info@gianlucabarranca.it>
2024-08-24 12:42:05 +0200

    add fbconfig on main.jsx

--- src/Routes/Admin.jsx
+++ src/Routes/Admin.jsx
@@ -1,4 +1,6 @@
 import { signOut, getAuth } from "firebase/auth";
+import CourseList from "../Components/Backend/CourseList";
+import AddCourse from "../Components/Backend/AddCourse";
 export function Admin() {
     const auth = getAuth();
 
@@ -12,6 +14,11 @@ export function Admin() {
     return (
         <div>
             <h1>This is the Admin page</h1>
+            <br></br>
+            <CourseList></CourseList>
+            <br />
+            <AddCourse></AddCourse>
+            <br />
             <button
                 onClick={() => {
                     handleSignOut();
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import fbconfig from './fbconfig.js'
-import { initializeApp } from 'firebase/app'
 
-const app = initializeApp(fbconfig)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
