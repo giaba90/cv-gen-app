baeb5309fa3bd9c9fedc5292949427d8fcee6bc7
Gianluca Barranca <info@gianlucabarranca.it>
2024-08-25 14:07:01 +0200

    add project form

--- src/Routes/Admin.jsx
+++ src/Routes/Admin.jsx
@@ -1,4 +1,7 @@
 import { signOut, getAuth } from "firebase/auth";
+import AddProjects from "../Components/Backend/AddProjetcs";
+import ProjectList from "../Components/Backend/ProjectsList";
+
 export function Admin() {
     const auth = getAuth();
 
@@ -12,6 +15,10 @@ export function Admin() {
     return (
         <div>
             <h1>This is the Admin page</h1>
+            <br />
+            <ProjectList></ProjectList>
+            <AddProjects></AddProjects>
+            <br />
             <button
                 onClick={() => {
                     handleSignOut();
