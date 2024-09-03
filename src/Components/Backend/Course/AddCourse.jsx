91022fc6c49c665007bbbd923d660bbce08f20ee
Gianluca Barranca <info@gianlucabarranca.it>
2024-09-03 19:52:31 +0200

    add formik to exsistent form

--- src/Components/Backend/Course/AddCourse.jsx
+++ src/Components/Backend/Course/AddCourse.jsx
@@ -3,6 +3,7 @@ import {
     Box,
     Button,
     CloseButton,
+    ModalCloseButton,
     FormControl,
     FormLabel,
     Input,
@@ -12,32 +13,24 @@ import {
     ModalHeader,
     ModalBody,
     ModalFooter,
-    Text,
     Textarea,
     VStack,
     HStack,
     useDisclosure,
+    Alert,
+    AlertIcon,
+    AlertTitle,
+    AlertDescription,
 } from "@chakra-ui/react";
 import { doc, collection, addDoc, serverTimestamp } from "firebase/firestore";
-import { db } from "../../../fbconfig" // Importa il db configurato
+import { db } from "../../../fbconfig"; // Importa il db configurato
+import { Formik, Form, Field } from "formik"; // Importa Formik
 
 const AddCourse = () => {
     const { isOpen, onOpen, onClose } = useDisclosure();
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [start, setStart] = useState("");
-    const [end, setEnd] = useState("");
-    const [school, setSchool] = useState("");
-    const [link, setLink] = useState("");
-    const [website, setWebsite] = useState("");
-    const [status, setStatus] = useState("");
-    const [loading, setLoading] = useState(false);
+    const [status, setStatus] = useState(null); // Stato per messaggi di successo o errore
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setLoading(true);
-        setStatus("");
-
+    const handleSubmit = async (values, actions) => {
         try {
             // Riferimento al documento 'education' nella raccolta principale
             const educationRef = doc(db, "db", "education");
@@ -47,134 +40,124 @@ const AddCourse = () => {
 
             // Aggiungi un nuovo documento alla raccolta 'courses'
             await addDoc(coursesCollectionRef, {
-                title,
-                description,
-                start,
-                end,
-                school,
-                link,
-                website,
+                ...values,
                 createdAt: serverTimestamp(), // Aggiungi un timestamp al documento
             });
 
-            setStatus("Corso aggiunto con successo!");
-            // Resetta i campi del form
-            setTitle("");
-            setDescription("");
-            setStart("");
-            setEnd("");
-            setSchool("");
-            setLink("");
-            setWebsite("");
-            onClose(); // Chiudi il modal dopo il successo
+            setStatus({ type: "success", message: "Corso aggiunto con successo!" });
+            actions.resetForm(); // Resetta i campi del form
+            onClose(); // Chiudi il modale dopo il successo
         } catch (error) {
             console.error("Errore durante l'inserimento del corso:", error);
-            setStatus("Errore durante l'inserimento del corso");
+            setStatus({
+                type: "error",
+                message: "Errore durante l'inserimento del corso",
+            });
         } finally {
-            setLoading(false);
+            actions.setSubmitting(false);
         }
     };
 
     return (
-        <Box>
+        <Box p={4}>
             <Button colorScheme="teal" onClick={onOpen}>
                 Aggiungi Corso
             </Button>
-            <Modal isOpen={isOpen} size="xl" onClose={onClose}>
+            <Modal isOpen={isOpen} onClose={onClose}>
                 <ModalOverlay />
                 <ModalContent>
-                    <ModalHeader>
-                        <HStack spacing={80}>
-                            <Text>Aggiungi Corso</Text>
-                            <CloseButton onClick={onClose} />
-                        </HStack>
-                    </ModalHeader>
+                    <ModalHeader>Aggiungi Corso</ModalHeader>
+                    <ModalCloseButton />
                     <ModalBody>
-                        <form onSubmit={handleSubmit}>
-                            <VStack spacing={4}>
-                                <FormControl isRequired>
-                                    <FormLabel>Titolo di studio:</FormLabel>
-                                    <Input
-                                        type="text"
-                                        value={title}
-                                        onChange={(e) => setTitle(e.target.value)}
-                                    />
-                                </FormControl>
-                                <FormControl isRequired>
-                                    <FormLabel>Scuola o Università:</FormLabel>
-                                    <Input
-                                        type="text"
-                                        value={school}
-                                        onChange={(e) => setSchool(e.target.value)}
-                                    />
-                                </FormControl>
-                                <FormControl isRequired>
-                                    <FormLabel>Sito Web:</FormLabel>
-                                    <Input
-                                        type="url"
-                                        value={website}
-                                        onChange={(e) => setWebsite(e.target.value)}
-                                    />
-                                </FormControl>
-                                <HStack spacing={48}>
-                                    <FormControl>
-                                        <FormLabel>Data Inizio:</FormLabel>
-                                        <Input
-                                            type="date"
-                                            value={start}
-                                            onChange={(e) => setStart(e.target.value)}
-                                        />
-                                    </FormControl>
-                                    <FormControl>
-                                        <FormLabel>Data Fine:</FormLabel>
-                                        <Input
-                                            type="date"
-                                            value={end}
-                                            onChange={(e) => setEnd(e.target.value)}
-                                        />
-                                    </FormControl>
-                                </HStack>
-                                <FormControl isRequired>
-                                    <FormLabel>Descrizione:</FormLabel>
-                                    <Textarea
-                                        value={description}
-                                        onChange={(e) => setDescription(e.target.value)}
-                                        placeholder="Inserisci una descrizione dettagliata"
-                                        size="md"
-                                    />
-                                </FormControl>
-                                <FormControl>
-                                    <FormLabel>Link:</FormLabel>
-                                    <Input
-                                        type="url"
-                                        value={link}
-                                        onChange={(e) => setLink(e.target.value)}
-                                    />
-                                </FormControl>
-                                <Button
-                                    colorScheme="teal"
-                                    type="submit"
-                                    isLoading={loading}
-                                    loadingText="Caricamento..."
-                                >
-                                    Salva Corso
-                                </Button>
-                            </VStack>
-                        </form>
-                    </ModalBody>
-                    <ModalFooter>
-                        {status && (
-                            <Text
-                                mt={4}
-                                color={status.includes("Errore") ? "red.500" : "green.500"}
-                            >
-                                {status}
-                            </Text>
-                        )}
+                        <Formik
+                            initialValues={{
+                                title: "",
+                                description: "",
+                                start: "",
+                                end: "",
+                                school: "",
+                                link: "",
+                                website: "",
+                            }}
+                            onSubmit={handleSubmit}
+                        >
+                            {(props) => (
+                                <Form>
+                                    <VStack spacing={4}>
+                                        <FormControl isRequired>
+                                            <FormLabel>Titolo di studio:</FormLabel>
+                                            <Field as={Input} name="title" type="text" />
+                                        </FormControl>
 
-                    </ModalFooter>
+                                        <FormControl isRequired>
+                                            <FormLabel>Scuola o Università:</FormLabel>
+                                            <Field as={Input} name="school" type="text" />
+                                        </FormControl>
+
+                                        <FormControl isRequired>
+                                            <FormLabel>Sito Web:</FormLabel>
+                                            <Field as={Input} name="website" type="url" />
+                                        </FormControl>
+                                        <FormControl>
+                                            <FormLabel>Data Inizio:</FormLabel>
+                                            <Field as={Input} name="start" type="date" />
+                                        </FormControl>
+                                        <FormControl>
+                                            <FormLabel>Data Fine:</FormLabel>
+                                            <Field as={Input} name="end" type="date" />
+                                        </FormControl>
+                                        <FormControl isRequired>
+                                            <FormLabel>Descrizione:</FormLabel>
+                                            <Field
+                                                as={Textarea}
+                                                name="description"
+                                                placeholder="Inserisci una descrizione dettagliata"
+                                                size="md"
+                                            />
+                                        </FormControl>
+
+                                        <FormControl>
+                                            <FormLabel>Link:</FormLabel>
+                                            <Field as={Input} name="link" type="url" />
+                                        </FormControl>
+                                    </VStack>
+                                    <ModalFooter>
+                                        <Button
+                                            colorScheme="teal"
+                                            mr={3}
+                                            type="submit"
+                                            isLoading={props.isSubmitting}
+                                        >
+                                            Salva corso
+                                        </Button>
+                                        <Button variant="ghost" onClick={onClose}>
+                                            Annulla
+                                        </Button>
+                                    </ModalFooter>
+                                </Form>
+                            )}
+                        </Formik>
+                    </ModalBody>
                 </ModalContent>
             </Modal>
+            {/* Messaggio di stato utilizzando il componente Alert di Chakra UI */}
+            {status && (
+                <Alert status={status.type} mt={4}>
+                    <AlertIcon />
+                    <Box flex="1">
+                        <AlertTitle>
+                            {status.type === "success" ? "Successo!" : "Errore!"}
+                        </AlertTitle>
+                        <AlertDescription>{status.message}</AlertDescription>
+                    </Box>
+                    <CloseButton
+                        position="absolute"
+                        right="8px"
+                        top="8px"
+                        onClick={() => setStatus(null)}
+                    />
+                </Alert>
+            )}
         </Box>
     );
 };
--- src/Components/Backend/Job/AddJob.jsx
+++ src/Components/Backend/Job/AddJob.jsx
@@ -1,18 +1,35 @@
 import { useState } from "react";
+import {
+    Box,
+    Button,
+    FormControl,
+    FormLabel,
+    Input,
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    VStack,
+    Alert,
+    AlertIcon,
+    AlertTitle,
+    AlertDescription,
+    CloseButton,
+    Textarea,
+} from "@chakra-ui/react";
 import { doc, collection, addDoc, serverTimestamp } from "firebase/firestore";
-import { db } from "../../../fbconfig" // Importa il db configurato
+import { db } from "../../../fbconfig"; // Importa il db configurato
+import { Formik, Form, Field } from "formik"; // Importa Formik
+
 const AddJob = () => {
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
-    const [start, setStart] = useState("");
-    const [end, setEnd] = useState("");
-    const [company, setCompany] = useState("");
-    const [website, setWebsite] = useState("");
-    const [status, setStatus] = useState("");
+    const [status, setStatus] = useState(null); // Stato per messaggi di successo o errore
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-
+    const handleSubmit = async (values, actions) => {
         try {
             // Riferimento al documento 'experience' nella raccolta principale
             const experienceRef = doc(db, "db", "experience");
@@ -22,88 +39,118 @@ const AddJob = () => {
 
             // Aggiungi un nuovo documento alla raccolta 'jobs'
             await addDoc(jobsCollectionRef, {
-                title: title,
-                description: description,
-                start: start,
-                end: end,
-                company: company,
-                website: website,
+                ...values,
                 createdAt: serverTimestamp(), // Aggiungi un timestamp al documento
             });
 
-            setStatus("Lavoro aggiunto con successo!");
-            // Resetta i campi del form
-            setTitle("");
-            setDescription("");
-            setStart("");
-            setEnd("");
-            setCompany("");
-            setWebsite("");
+            setStatus({ type: "success", message: "Lavoro aggiunto con successo!" });
+            actions.resetForm(); // Resetta i campi del form con Formik
+            onClose(); // Chiudi il modale
         } catch (error) {
             console.error("Errore durante l'inserimento del Lavoro:", error);
-            setStatus("Errore durante l'inserimento del Lavoro");
+            setStatus({
+                type: "error",
+                message: "Errore durante l'inserimento del Lavoro",
+            });
         }
     };
 
     return (
-        <div>
-            <h1>Aggiungi Lavoro</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Posizione lavorativa:</label>
-                    <input
-                        type="text"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        required
+        <Box p={4}>
+            {/* Pulsante per aprire il modale */}
+            <Button colorScheme="teal" onClick={onOpen}>
+                Aggiungi Lavoro
+            </Button>
+
+            {/* Modale per il form di aggiunta del lavoro */}
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Aggiungi Lavoro</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        {/* Utilizza Formik per gestire il form */}
+                        <Formik
+                            initialValues={{
+                                title: "",
+                                company: "",
+                                description: "",
+                                start: "",
+                                end: "",
+                                website: "",
+                            }}
+                            onSubmit={handleSubmit}
+                        >
+                            {(props) => (
+                                <Form>
+                                    <VStack spacing={4} align="stretch">
+                                        <FormControl isRequired>
+                                            <FormLabel>Posizione lavorativa</FormLabel>
+                                            <Field as={Input} name="title" type="text" />
+                                        </FormControl>
+
+                                        <FormControl isRequired>
+                                            <FormLabel>Azienda</FormLabel>
+                                            <Field as={Input} name="company" type="text" />
+                                        </FormControl>
+                                        <FormControl>
+                                            <FormLabel>Sito Web</FormLabel>
+                                            <Field as={Input} name="website" type="url" />
+                                        </FormControl>
+                                        <FormControl>
+                                            <FormLabel>Data Inizio</FormLabel>
+                                            <Field as={Input} name="start" type="date" />
+                                        </FormControl>
+                                        <FormControl>
+                                            <FormLabel>Data Fine</FormLabel>
+                                            <Field as={Input} name="end" type="date" />
+                                        </FormControl>
+                                        <FormControl isRequired>
+                                            <FormLabel>Descrizione</FormLabel>
+                                            <Field as={Textarea} name="description"
+                                                placeholder="Inserisci una descrizione dettagliata"
+                                                size="md" />
+                                        </FormControl>
+                                    </VStack>
+                                    <ModalFooter>
+                                        <Button
+                                            colorScheme="teal"
+                                            mr={3}
+                                            type="submit"
+                                            isLoading={props.isSubmitting}
+                                        >
+                                            Salva lavoro
+                                        </Button>
+                                        <Button variant="ghost" onClick={onClose}>
+                                            Annulla
+                                        </Button>
+                                    </ModalFooter>
+                                </Form>
+                            )}
+                        </Formik>
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
+
+            {/* Messaggio di stato utilizzando il componente Alert di Chakra UI */}
+            {status && (
+                <Alert status={status.type} mt={4}>
+                    <AlertIcon />
+                    <Box flex="1">
+                        <AlertTitle>
+                            {status.type === "success" ? "Successo!" : "Errore!"}
+                        </AlertTitle>
+                        <AlertDescription>{status.message}</AlertDescription>
+                    </Box>
+                    <CloseButton
+                        position="absolute"
+                        right="8px"
+                        top="8px"
+                        onClick={() => setStatus(null)}
                     />
-                </div>{" "}
-                <div>
-                    <label>Azienda:</label>
-                    <input
-                        type="text"
-                        value={company}
-                        onChange={(e) => setCompany(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Descrizione:</label>
-                    <input
-                        type="text"
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Data Inizio:</label>
-                    <input
-                        type="date"
-                        value={start}
-                        onChange={(e) => setStart(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label>Data Fine:</label>
-                    <input
-                        type="date"
-                        value={end}
-                        onChange={(e) => setEnd(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label>Sito Web:</label>
-                    <input
-                        type="url"
-                        value={website}
-                        onChange={(e) => setWebsite(e.target.value)}
-                    />
-                </div>
-                <button type="submit">Aggiungi Lavoro</button>
-            </form>
-            {status && <p>{status}</p>}
-        </div>
+                </Alert>
+            )}
+        </Box>
     );
 };
 
