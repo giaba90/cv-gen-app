9e6ad7df5e8237e126473330defd8a71e788382c
Gianluca Barranca <info@gianlucabarranca.it>
2024-09-11 09:35:25 +0200

    fix 404 error on netlify

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
   define: {
     'process.env': {
