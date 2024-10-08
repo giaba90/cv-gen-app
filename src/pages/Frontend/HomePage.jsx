bf89d7a8f07f158dc28d5904d6bc35da2813905a
Gianluca Barranca <info@gianlucabarranca.it>
2024-10-08 17:34:50 +0200

    I made the homepage responsive

--- src/pages/Frontend/HomePage.jsx
+++ src/pages/Frontend/HomePage.jsx
@@ -1,4 +1,4 @@
-import { ChakraProvider, Box, Grid, Container, theme, extendTheme } from '@chakra-ui/react';
+import { ChakraProvider, Box, Flex, Container, theme, extendTheme } from '@chakra-ui/react';
 import Hero from '../../components/Hero'
 import Summary from '../../components/Summary';
 import Contact from '../../components/Frontend/Contact';
@@ -27,19 +27,19 @@ function HomePage() {
     <ChakraProvider theme={customTheme}>
       <Box minHeight="100vh" bg="gray.100" py={8}>
         <Container maxW="container.xl">
-          <Grid templateColumns="30% 70%">
-            <Box bg="#005C85" color="white" p={6}>
+          <Flex direction={{ base: "column", md: "row" }} gap={4}>
+            <Box bg="#005C85" color="white" p={6} flexBasis={{ base: "100%", md: "30%" }}>
               <Contact />
               <Skills />
             </Box>
-            <Box bg="white" p={6}>
+            <Box bg="white" p={6} flexBasis={{ base: "100%", md: "70%" }}>
               <Hero />
               <Summary/>
               <Experience />      
               <Education />
               <Projects />
             </Box>
-          </Grid>
+          </Flex>
         </Container>
         <Footer />
       </Box>
